87b12c98f682bf45f39dd3af30d342c22f683ea5
unknown <yjs12180825@gmail.com>
2023-08-29 06:04:42 +0900

    commit

--- controller/controller.js
+++ controller/controller.js
@@ -8,17 +8,17 @@ const chatbotInfo = [
 
 ]
 
-// const addingPrompt = [...chatbotInfo];
+const addingPrompt = [...chatbotInfo];
 
-// const storeUserQuestion = (message) => {
-//     addingPrompt.push({ role: 'user', content: message });
-//     return addingPrompt;
-// }
+const storeUserQuestion = (message) => {
+    addingPrompt.push({ role: 'user', content: message });
+    return addingPrompt;
+}
 
-// const storeGPTResponse = (response) => {
-//     addingPrompt.push({ role: 'assistant', content: response });
-//     return addingPrompt;
-// }
+const storeGPTResponse = (response) => {
+    addingPrompt.push({ role: 'assistant', content: response });
+    return addingPrompt;
+}
 
 exports.callChatGpt = async (text) => {
     // console.log(...addingPrompt)
@@ -26,7 +26,7 @@ exports.callChatGpt = async (text) => {
         const chatCompletion = await openai.createChatCompletion({
             model: "gpt-3.5-turbo",
             messages: [
-                ...chatbotInfo,
+                ...addingPrompt,
                 {
                     role: 'assistant', content: "Chatbot to eradicate drug addiction"
                 },
